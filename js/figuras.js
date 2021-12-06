c27cbadabfa7902d849994afdc7fca3400583f19
Luis Rosales <luisrosales@Luiss-MacBook-Pro.local>
2021-12-06 16:28:16 -0600

    curso terminado

--- js/figuras.js
+++ js/figuras.js
@@ -1,49 +1,160 @@
 // Area y Perimetro del Cuadrado
-console.group("Cuadrado")
+function perimetroCuadrado(lado){
+    return lado * 4
+}
 
-const ladoCuadrado = 5;
-const perimetroCuadrado = ladoCuadrado * 4;
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-
-console.log(`El Lado del Cuadrado mide ${ladoCuadrado} Cm`)
-console.log(`El Perimetro del cuadrado es ${perimetroCuadrado} Cm`)
-console.log(`El area del cuadrado es ${areaCuadrado} Cm^2`)
+function areaCuadrado(lado){
+    return lado * lado
+}
 
 console.groupEnd()
 
-
 // Area y perimetro de Triangulo
+
 console.group("Triangulo")
 
 
-const ladoUnoTriangulo = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5;
-const perimetroTriangulo = ladoUnoTriangulo + ladoUnoTriangulo + baseTriangulo;
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+function perimetroTriangulo(lado1, lado2, base){
+  return lado1 + lado2 + base
+}
 
-console.log(`El lado del triangulo mide ${ladoUnoTriangulo} cm`)
-console.log(`La base del triangulo mide ${baseTriangulo} Cm`)
-console.log(`El perimetro del triangulo es ${perimetroTriangulo} `)
-console.log(`El area del Triangulo es ${areaTriangulo} Cm^2`)
+function areaTriangulo(base, altura){
+    return (base * altura) / 2
+}
 
 console.groupEnd()
 
 // Radio, Area y Circuferencia del Ciruclo
 console.group("Circulo")
 
-const radio = 4;
 const PI = Math.PI;
-const diametro = radio * 2;
-const circunferencia =  diametro  * PI ;
-const areaCirculo = PI * (radio*radio);
 
-console.log(`El Radio es ${radio} Cm`)
-console.log(`El Diametro es  ${diametro} Cm`)
-console.log(`La circunferencia es  ${circunferencia} Cm`)
-console.log(`El area es  ${areaCirculo} Cm^2`)
+function circunferencia(radio){
+  return  (radio * 2) * PI
+}
+
+function areaCirculo(radio){
+   return `El area del Ciruclo es: ${PI * (radio * radio)} CM^2`
+}
 
 console.groupEnd()
 
+// // Web app
+
+// // Perimetro de un Cuadrado
+ function calcularPerimetro(){
+
+    const ladoElemento = document.getElementById("ladoCuadrado")
+
+    const lado1 = ladoElemento.value
+
+    alert(perimetroCuadrado(lado1))
+
+}
+
+// Area de un Cuadrado
+function calcularArea(){
+
+    const ladoElemento = document.getElementById("ladoCuadrado")
+
+    const lado = ladoElemento.value
+
+    alert(areaCuadrado(lado))
+
+}
+
+// Perimetro de un Triangulo en Funciones conectadas a HTML
+
+  const calcularPerimetroTriangulo = () => {
+
+    const ladoUnoTriangulo = document.getElementById("ladoUnoTriangulo")
+    var valorLadoUno = Number(ladoUnoTriangulo.value)
+
+    const ladoDosTriangulo = document.getElementById("ladoDosTriangulo")
+    var valorLadoDos = Number(ladoDosTriangulo.value)
+
+    const baseTriangulo = document.getElementById("baseTriangulo")
+    var valorbaseTriangulo = Number(baseTriangulo.value)
+
+    const perimetroT = perimetroTriangulo(valorLadoUno, valorLadoDos, valorbaseTriangulo)
+
+    alert(perimetroT)
+
+  }
+
+ const calcularAreaTriangulo = () => {
+
+  const baseElem = document.getElementById("baseTriangulo")
+  const base = baseElem.value
+
+  const alturaElem = document.getElementById("alturaTriangulo")
+  const altura = alturaElem.value
+
+  alert(areaTriangulo(base, altura))
+  }
+
+// Calcular Diametro y Area de Circulo en pantalla
+
+
+const calcularCircunferencia = () => {
+
+  const radioInput = document.getElementById("radioInput")
+  const radio = radioInput.value
+
+ alert(circunferencia(radio)) 
+}
+
+const calcularAreaCirculo = () => {
+
+  const radioInput = document.getElementById("radioInput")
+  const radio = radioInput.value
+
+  alert(areaCirculo(radio))
+
+}
+
+// TRIANGULO ISOSELES
+
+/*
+## En este ejercicio debes crear una función para calcular la altura de un triángulo isósceles.
+
+##La función debe recibir, como parámetros, la longitud de los 3 lados del triángulo.
+
+## La función debe validar que la longitud de los 3 lados del triángulo corresponden a un triángulo isósceles.
+
+## La función debe retornar la altura del triángulo.
+
+Pista: la función Math.sqrt de JavaScript puede ayudarte a calcular raíces cuadradas.
+*/
+
+const calcularAlturaIsoseles = (ladoUno,ladoDos, base) => {
+
+  const ladoUnoElem = document.getElementById("ladoUnoIsoseles")
+  const ladoUnoIsoseles = ladoUnoElem.value
+
+  const ladoDosElem = document.getElementById("ladoDosIsoseles")
+  const ladoDosIsoseles = ladoDosElem.value
+
+  const baseElem = document.getElementById("baseIsoseles")
+  const baseIsoseles = baseElem.value
+
+  if(ladoUnoIsoseles == ladoDosIsoseles){
+    const catetoDos = baseIsoseles / 2
+    const hipotenusa = ladoUnoIsoseles 
+  
+    // Formula para calcular cateto 1 de triangulo rectangulo
+    const hipotenusaCuadrado = hipotenusa * hipotenusa
+    const catetoDosCuadrado = catetoDos * catetoDos
+  
+    const catetoUno = Math.sqrt(hipotenusaCuadrado - catetoDosCuadrado) 
+  
+    alert(catetoUno)
+
+  }
+  else{
+    alert("Lado uno y lado dos del Triangulo deben ser iguales")
+  }
+
+}
 
 
